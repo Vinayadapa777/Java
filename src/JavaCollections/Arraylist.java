94f3f5fac47500f59b13d47017f6f75d4726ac6d
unknown <vinayadapa7777@gmail.com>
2023-11-23 16:25:20 +0530

    Adding arraylist concepts

---
+++ src/JavaCollections/Arraylist.java
@@ -0,0 +1,89 @@
+package JavaCollections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Arraylist {
+    public static void main(String args[]) {
+	ArrayList<Object> al = new ArrayList<Object>();
+	al.add("Welcome");
+	al.add(7);
+	al.add(7.000);
+	al.add('A');
+	al.add(true);
+	System.out.println("Printing the List : " + al);// [Welcome, 7, 7.0, A, true]
+	System.out.println("Size of an ArrayList : " + al.size());// 5
+	al.remove(0);
+	System.out.println("After removing the 0 Index : " + al);// [7, 7.0, A, true]
+	al.add(2, "Welcome");
+	System.out.println("After adding the Welcome in index 2 : " + al);// [7, 7.0, Welcome, A, true]
+	System.out.println("getting particular element ex:index2 : " + al.get(2));// Welcome
+	al.set(2, "Replacing");
+	System.out.println("After replacing the particular Index : " + al);// [7, 7.0, Replacing, A, true]
+	System.out.println("Searching element is contains :" + al.contains(7));// true
+	System.out.println("Searching element is notcontains :" + al.contains(71));// false
+	System.out.println("Checking List is empty or not :" + al.isEmpty());// false
+	System.out.println();
+	// Reading the data from different approaches
+
+	// 1.forLoop
+	System.out.println("Reading the list using for loop.......");
+	for (int i = 0; i < al.size(); i++) {
+	    System.out.print(al.get(i) + " ");// 7 7.0 Replacing A true
+	}
+	// 2.foreach
+	System.out.println();
+	System.out.println("Readimg the list using foreach loop.......");
+	for (Object a : al) {
+	    System.out.print(a + " ");// 7 7.0 Replacing A true
+	}
+	System.out.println();
+	System.out.println("Reading the list using iterator loop.......");
+	Iterator<Object> it = al.iterator();
+	while (it.hasNext()) {
+	    System.out.print(it.next() + " ");
+	}
+	System.out.println();
+
+	// Other Examples
+	ArrayList<String> ax = new ArrayList<String>();
+	ax.add("A");
+	ax.add("G");
+	ax.add("C");
+	ax.add("Z");
+	ax.add("B");
+	System.out.println("Printing ax list" + ax);//[A, G, C, Z, B]
+	// adding the ax to new ArrayList
+	ArrayList<Object> dup = new ArrayList<Object>();
+	dup.addAll(ax);
+	System.out.println("Print after adding the list to dup :" + dup);//[A, G, C, Z, B]
+	dup.removeAll(dup);
+	System.out.println("Print after removing the list to dup :" + dup);//[]
+
+	//Sorting the list of Elements in Ascending Order
+	System.out.println("Printing ax list Before Sorting" + ax);//[A, G, C, Z, B]
+	Collections.sort(ax);
+	System.out.println("Printing ax list After sorting" + ax);//[A, B, C, G, Z]
+	//sorting the list of Elements in reverse Order
+	Collections.sort(ax,Collections.reverseOrder());
+	System.out.println("Printing ax list After sorting Descending Order" + ax);//[Z, G, C, B, A]
+	//Shuffling all the List
+	Collections.shuffle(ax);
+	System.out.println("Print ax list after shuffling :" + ax);//shuffled elements
+	
+	//Converting the Normal Array to ArrayList
+	String arr[]= {"Vinay", "Aksha", "Vihan"};
+	for(String s:arr) {
+	    System.out.print(s+ " ");
+	}
+	System.out.println();//Vinay Aksha Vihan
+	System.out.println("After converting Normal Array to ArrayList");
+	ArrayList <Object> alc=new ArrayList<Object>(Arrays.asList(arr));
+	System.out.println(alc);//[Vinay, Aksha, Vihan]
+	
+	
+	
+    }
+}
---
+++ src/JavaCollections/NormalArray.java
@@ -0,0 +1,28 @@
+package JavaCollections;
+
+public class NormalArray {
+
+    public static void main(String[] args) {
+	int a[] = new int[5];
+	a[0] = 1;
+	a[1] = 2;
+	a[2] = 3;
+	a[3] = 4;
+	a[4] = 5;
+	int b[] = { 1, 2, 3, 4, 5, 6, 7 };
+	System.out.println("Printing Array a");
+	for (int i = 0; i < a.length; i++) {
+	    System.out.print(a[i] + " ");
+	}
+	System.out.println();
+	System.out.println("Printing Array b");
+	for (int i = 0; i < a.length; i++) {
+	    System.out.print(b[i] + " ");
+	}
+	System.out.println();
+	System.out.println("using for each loop");
+	for (int i : b) {
+	    System.out.print(i + " ");
+	}
+    }
+}
