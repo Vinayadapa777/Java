c74dd11aa65bbe9f68a184bc39d8a78541f76e77
VinayAdapa <vinayadapa7777@gmail.com>
2024-12-02 10:19:05 +0530

    gsfd

--- src/Dummy/DummyClass.java
+++ src/Dummy/DummyClass.java
@@ -5,7 +5,7 @@ public class DummyClass {
     public static void main(String[] args) {
 	int i=1;
 	int h=5;
-	System.out.println(228);
+	System.out.println("sysobsjnd");
 	System.out.println(56324+5243654);
 	System.out.println("conflicts resolving ");
 	System.out.println("sbfdcbs stash ");
